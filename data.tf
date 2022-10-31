e88de7b756501df1e1d9817fcac67c23c7b36d83
AWankh <90633246+AWankh@users.noreply.github.com>
2022-10-31 08:26:16 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,11 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  name_regex  = "amzn2-ami-hvm*"
+}
+
+data "aws_caller_identity" "current" {}
